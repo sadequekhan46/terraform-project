5aabd03245844c57e20c5d6612a98f3c4415a9c9
sadequekhan <sadequekhan46@gmai.com>
2026-04-03 03:21:10 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -34,6 +34,46 @@ resource "aws_instance" "infrastructure-as-code" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private.id
 }
+resource "aws_launch_template" "web" {
+  name_prefix   = "web-server-"
+  image_id      = "ami-0c02fb55956c7d316"   # Use your correct US-East-1 AMI
+  instance_type = "t2.micro"
+
+  key_name = "iac-key-pair"   # optional
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "Terraform-EC2"
+    }
+  }
+}
+resource "aws_autoscaling_group" "asg" {
+  name                      = "asg-web"
+  max_size                  = 2
+  min_size                  = 1
+  desired_capacity          = 1
+  vpc_zone_identifier       = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+}
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+
+  subnets = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+  security_groups = [aws_security_group.lb_sg.id]
+}
 
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
