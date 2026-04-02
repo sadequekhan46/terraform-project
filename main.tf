8d0dbfc832a1f0f515887dc8c46661fda591fd46
sadequekhan <sadequekhan46@gmai.com>
2026-04-03 02:13:56 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -30,20 +30,20 @@ resource "aws_security_group" "web_sg" {
   }
 }
 resource "aws_instance" "infrastructure-as-code" {
-  ami           = "ami-xxxx"
+  ami           = "ami-ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private.id
 }
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-1a"
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.3.0/24"
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-1b"
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.4.0/24"
 }
 resource "aws_db_instance" "db" {
   engine         = "mysql"
