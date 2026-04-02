4a4832e7d90db610f8b6f7584936931f55c61f8b
sadequekhan <sadequekhan46@gmai.com>
2026-04-02 21:57:16 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_eip" "nat" {}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+}
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "iac-project" {
+  ami           = "ami-0312899aaa46bc865"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.private.id
+}
+resource "aws_lb" "alb" {
+  subnets = [aws_subnet.public.id]
+}
+resource "aws_db_instance" "db" {
+  engine         = "mysql"
+  instance_class = "db.t3.micro"
+  allocated_storage = 20
+  username = "admin"
+  password = "password123"
+}
