4067501ed428b2b036b6710ae5345c783c8da350
sadequekhan <sadequekhan46@gmai.com>
2026-04-03 03:30:50 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -48,6 +48,21 @@ resource "aws_launch_template" "web" {
     }
   }
 }
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
 resource "aws_autoscaling_group" "asg" {
   name                      = "asg-web"
   max_size                  = 2
@@ -57,13 +72,32 @@ resource "aws_autoscaling_group" "asg" {
 
   launch_template {
     id      = aws_launch_template.web.id
-    version = "$Latest"
+    version = "$aws_launch_template.web.latest_version"
   }
 
   target_group_arns = [aws_lb_target_group.app_tg.arn]
 
   health_check_type         = "EC2"
   health_check_grace_period = 300
+}
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  description = "Security group for Load Balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 resource "aws_lb" "app_lb" {
   name               = "app-lb"
